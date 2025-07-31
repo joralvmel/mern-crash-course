2681a2946273221ac0c99df723c5357c102ad3b9
Jorge Andres Alvarez Melchor <joralvmel@gmail.com>
2025-07-31 16:05:52 +0200

    chore: add zustand store and page implementation

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,6 @@
-import {Box, Button, Container, Heading, Input, useColorModeValue, VStack} from "@chakra-ui/react";
+import {Box, Button, Container, Heading, Input, useColorModeValue, useToast, VStack} from "@chakra-ui/react";
 import {useState} from "react";
+import {useProductStore} from "../store/product.js";
 
 const CreatePage = () => {
     const [newProduct, setNewProduct] = useState({
@@ -8,9 +9,29 @@ const CreatePage = () => {
         image: "",
     });
 
-    const handleAddProduct = () => {
-        console.log("New Product:", newProduct);
-    }
+    const toast = useToast()
+
+    const {createProduct} = useProductStore();
+
+    const handleAddProduct = async () => {
+        const { success, message } = await createProduct(newProduct);
+        if (!success) {
+            toast({
+                title: "Error",
+                description: message,
+                status: "error",
+                isClosable: true,
+            });
+        } else {
+            toast({
+                title: "Success",
+                description: message,
+                status: "success",
+                isClosable: true,
+            });
+        }
+        setNewProduct({ name: "", price: "", image: "" });
+    };
 
   return (
       <Container maxW={"container.sm"}>
